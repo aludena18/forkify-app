928e38691213cf3da2387204888ee4a5568fb772
aludena18 <aludena18@gmail.com>
2022-12-28 10:41:26 -0500

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+// export const KEY = 'e99ddb66-5592-4886-9835-90f1656e3b53';
+export const KEY = '0d08df5f-7d75-4b54-ad46-d04444c7be40';
+export const MODAL_CLOSE_SEC = 2.5;
+
+// POST
+//https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=<insert your key>
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,81 @@
+import View from './View';
+import icons from 'url:../../img/icons.svg'; //Parcel 2
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkup() {
+    const curPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+
+    //1 page and others
+    if (curPage === 1 && numPages > 1)
+      return `
+        <button data-goto="${
+          curPage + 1
+        }" class="btn--inline pagination__btn--next">
+            <span>Page ${curPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+         </button>
+    `;
+
+    //last page
+    if (curPage === numPages && numPages > 1)
+      return `
+        <button data-goto="${
+          curPage - 1
+        }" class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+        </button>
+    `;
+
+    //other page
+    if (curPage < numPages)
+      return `
+        <button data-goto="${
+          curPage - 1
+        }" class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+        </button>
+        <button data-goto="${
+          curPage + 1
+        }" class="btn--inline pagination__btn--next">
+            <span>Page ${curPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+         </button>
+    `;
+
+    //1 page and NO more
+    return '';
+  }
+
+  _generateMarkupButton() {
+    //For challenge. Refactor the code of generateMarkup
+  }
+}
+
+export default new PaginationView();
